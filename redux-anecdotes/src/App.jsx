592eb82fd1265d91cb131358c5ffd167e4ec7fb4
ParadkarPrasad <prasadparad37@gmail.com>
2024-09-04 18:42:33 +0100

    Exercise 6.5 - 6.8

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,38 +1,37 @@
-import { useSelector, useDispatch } from 'react-redux'
-import {updateVote, addAnecdote} from "./reducers/anecdoteReducer"
-const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+// import { useSelector, useDispatch } from 'react-redux'
+// import {updateVote} from "./reducers/anecdoteReducer"
 
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(updateVote(id))
-  }
-const addNote = (event)=>{
-  event.preventDefault()
-  const content = event.target.anecdote.value;
-  event.target.anecdote.value = ''
-  dispatch(addAnecdote(content))
-}
+import ListAnecdote from "./components/ListAnecdote"
+import AnecdoteForm from "./components/AnecdoteForm"
+const App = () => {
+  // const anecdotes = useSelector(state => state)
+  // const dispatch = useDispatch()
+
+  // const vote = (id) => {
+  //   console.log('vote', id)
+  //   dispatch(updateVote(id))
+  // }
+
   return (
-    <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
-      <form onSubmit={addNote}>
-        <div><input name="anecdote" /></div>
-        <button type='submit'>create</button>
-      </form>
+    // <div>
+    //   <h2>Anecdotes</h2>
+    //   {anecdotes.map(anecdote =>
+    //     <div key={anecdote.id}>
+    //       <div>
+    //         {anecdote.content}
+    //       </div>
+    //       <div>
+    //         has {anecdote.votes}
+    //         <button onClick={() => vote(anecdote.id)}>vote</button>
+    //       </div>
+    //     </div>
+    //   )}
+      
+    // </div>
+<div>
+    <h2>Anecdotes</h2>
+    <ListAnecdote/>
+  <AnecdoteForm/>
     </div>
   )
 }
